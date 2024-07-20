967ced675cfd588c560462984457eeff648f6753
D_M <a89297175356@gmail.com>
2024-07-20 15:01:33 +0500

    Python 3 message

--- example1.py
+++ example1.py
@@ -1,20 +1,15 @@
 n = int(input())
-sum = ''
-for i in range(1, n + 1):
-    total = 0
-    sum = ''
-    for j in range(1, i + 1):
-        if i % j == 0:
-            sum += '+'
-    print(i, sum, sep='')
+if n < 9:
+    print(n)
+else:
+    while n > 9:
+        sum = 0
+        while (n) > 0:
+            sum += n % 10
+            n = n // 10
+        n = sum
+    print(sum)
 
-
-# Sample Input 1:
-# 1
-# 10
-# Sample Output 1:
-# 10 18
-# Sample Input
 
 # a, b, c, d = int(input()), int(input()), int(input()), int(input())
 # a, b, c, d = float(input()), float(input()), float(input()), float(input())
