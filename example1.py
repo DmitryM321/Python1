0ac74073f13ee0444f7f66b52e4a1ce75f4b51ac
D_M <a89297175356@gmail.com>
2024-07-28 15:18:22 +0500

    Python 3 message

--- example1.py
+++ example1.py
@@ -1,18 +1,37 @@
-a, b = int(input()), int(input())
-total = 0
-for i in range (a, b + 1):
-        total = 0
+n = int(input())
+count3 = 0
+count_last_dig = 0
+even = 0
+sum_more5 = 0
+deterioration_more7 = 1
+count_0_and_5 = 0
+a1 = n % 10
+while n > 0:
+    a = n % 10
+    n = n // 10
+    if a == 3:
+        count3 += 1
+    if a1 == a:
+        count_last_dig += 1
+    if a % 2 == 0:
+        even += 1
+    if a > 5:
+        sum_more5 += a
+    if a == 0 or a == 5:
+        count_0_and_5 += 1
+    if a > 7:
+        deterioration_more7 *= a
+    if deterioration_more7 == 7:
+        deterioration_more7 = 7
+        if a != 7:
+            deterioration_more7 = 1
 
-        for j in range (1, i + 1):
-                if i % (j) == 0:
-                        total += 1
-        if total == 2 and i > 1:
-                print(i)
-
-
-
-
-
+print(count3)
+print(count_last_dig)
+print(even)
+print(sum_more5)
+print(deterioration_more7)
+print(count_0_and_5)
 
 # a, b, c, d = int(input()), int(input()), int(input()), int(input())
 # a, b, c, d = float(input()), float(input()), float(input()), float(input())
